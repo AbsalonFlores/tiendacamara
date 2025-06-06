dd0c16308baea84ffb90277c928c60f2acd8083c
AbsalonFlores <absaflores@hotmail.com>
2025-06-06 16:57:16 -0600

    Utils

---
+++ sendgrid.js
@@ -0,0 +1,2 @@
+exports.sendPurchaseEmail = async (email, nombre, total) => { /* SendGrid email code */ };
+exports.sendInvitationEmail = async (email, producto) => { /* SendGrid invitation code */ };
